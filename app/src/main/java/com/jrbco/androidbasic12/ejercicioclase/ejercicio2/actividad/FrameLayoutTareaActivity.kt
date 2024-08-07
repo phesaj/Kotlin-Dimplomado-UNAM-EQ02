c68ee14ac96f9e200c2778d28f8935d316c3377a
juan <juanjosegonzalesmelgarejo@gmail.com>
2024-08-07 14:34:40 -0600

    mejora de Relative

--- app/src/main/java/com/jrbco/androidbasic12/ejercicioclase/ejercicio2/actividad/FrameLayoutTareaActivity.kt
+++ app/src/main/java/com/jrbco/androidbasic12/ejercicioclase/ejercicio2/actividad/FrameLayoutTareaActivity.kt
@@ -1,6 +1,10 @@
 package com.jrbco.androidbasic12.ejercicioclase.ejercicio2.actividad
 
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import android.widget.Button
+import android.widget.Spinner
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -12,10 +16,29 @@ class FrameLayoutTareaActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_frame_layout_tarea)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        val desayunoSpinner = findViewById<Spinner>(R.id.spinDesayuno)
+        val comidaSpinner = findViewById<Spinner>(R.id.spinComida)
+        val btndesayuno = findViewById<Button>(R.id.btnDesayunoOrder)
+        val btncomida = findViewById<Button>(R.id.btnComidaOrder)
+
+        val cantidades = arrayListOf("1", "2", "3", "4", "5")
+
+        val adapter = ArrayAdapter(this, android.R.layout.simple_spinner_item, cantidades)
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+
+        desayunoSpinner.adapter = adapter
+        comidaSpinner.adapter = adapter
+
+        btndesayuno.setOnClickListener {
+            val itemSpinnerSelected = desayunoSpinner.selectedItem
+            Toast.makeText(this, "Se agregaron al carrito $itemSpinnerSelected desayunos", Toast.LENGTH_SHORT).show()
         }
+
+        btncomida.setOnClickListener {
+            val itemSpinnerSelected = comidaSpinner.selectedItem
+            Toast.makeText(this, "Se agregaron al carrito $itemSpinnerSelected comidas", Toast.LENGTH_SHORT).show()
+        }
+
     }
 }
