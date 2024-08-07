315d36375788adb0f45eb3a74e9cab5825a08793
juan <juanjosegonzalesmelgarejo@gmail.com>
2024-08-06 21:35:59 -0600

    Creacion del punto 1 y 3

---
+++ app/src/main/java/com/jrbco/androidbasic12/ejercicioclase/ejercicio2/actividad/RelativeLayoutTareaActivity.kt
@@ -0,0 +1,21 @@
+package com.jrbco.androidbasic12.ejercicioclase.ejercicio2.actividad
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.jrbco.androidbasic12.R
+
+class RelativeLayoutTareaActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_relative_layout_tarea)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
